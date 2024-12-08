9279c1d9710b250827766440599d101f4bf725bd
masx200 <masx200@qq.com>
2024-12-08 23:04:16 +0800

    Update Pendant.kt

--- src/main/java/com/github/masx200/biliClient/model/user/Pendant.kt
+++ src/main/java/com/github/masx200/biliClient/model/user/Pendant.kt
@@ -7,35 +7,46 @@ package com.github.masx200.biliClient.model.user
  * @version 1.0 2021-02-07-14:17
  * @since 2021-02-07-14:17
  */
-data //@com.alibaba.fastjson2.annotation.JSONCompiled
-class Pendant {
+
+
+import kotlinx.serialization.Serializable
+
+/**
+ * 描述： 头像框信息
+ *
+ * @author lpc lpc@hll520.cn
+ * @version 1.0 2021-02-07-14:17
+ * @since 2021-02-07-14:17
+ */
+@Serializable
+data class Pendant(
     /**
      * 头像框ID
      */
-    private val pid: Long? = null
+    val pid: Long? = null,
 
     /**
      * 头像框名称
      */
-    private val name: String? = null
+    val name: String? = null,
 
     /**
      * 头像框URL
      */
-    private val image: String? = null
+    val image: String? = null,
 
     /**
      * 到期时间
      */
-    private val expire: Long? = null
+    val expire: Long? = null,
 
     /**
      * 图形画质提升
      */
-    private val image_enhance: String? = null
+    val image_enhance: String? = null,
 
     /**
      * 图形画质提升框架
      */
-    private val image_enhance_frame: String? = null
-}
+    val image_enhance_frame: String? = null
+)
