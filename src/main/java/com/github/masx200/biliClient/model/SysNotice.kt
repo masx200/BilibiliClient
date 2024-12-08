553e742cf6f71429ce50de7c9060d5424280f715
masx200 <masx200@qq.com>
2024-12-08 23:01:11 +0800

    Update SysNotice.kt

--- src/main/java/com/github/masx200/biliClient/model/SysNotice.kt
+++ src/main/java/com/github/masx200/biliClient/model/SysNotice.kt
@@ -1,5 +1,7 @@
 package com.github.masx200.biliClient.model
 
+import kotlinx.serialization.Serializable
+
 /**
  * 描述： 系统通知信息
  *
@@ -7,22 +9,22 @@ package com.github.masx200.biliClient.model
  * @version 1.0 2021-02-07-14:23
  * @since 2021-02-07-14:23
  */
-data //@com.alibaba.fastjson2.annotation.JSONCompiled
-class SysNotice {
+@Serializable
+data class SysNotice(
     /**
      * 通知id
      */
-    private val id: Int? = null
+    val id: Int? = null,
 
     /**
      * 提示信息
      */
-    private val content: String? = null
+    val content: String? = null,
 
     /**
      * 详细说明连接
      */
-    private val url: String? = null
+    val url: String? = null,
 
     /**
      * 通知类型
@@ -31,20 +33,20 @@ class SysNotice {
      * 2 纪念账号
      *
      */
-    private val notice_type: Int? = null
+    val notice_type: Int? = null,
 
     /**
      * 通知图标连接
      */
-    private val icon: String? = null
+    val icon: String? = null,
 
     /**
      * 文本颜色 #16进制
      */
-    private val text_color: String? = null
+    val text_color: String? = null,
 
     /**
      * 背景颜色 #16进制
      */
-    private val bg_color: String? = null
-}
+    val bg_color: String? = null
+)
