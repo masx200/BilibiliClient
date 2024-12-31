bcaac70a8b4dec65485e227a7aa49f79f5a84751
masx200 <masx200@qq.com>
2024-12-31 17:28:11 +0800

    不用referer

--- src/main/kotlin/com/github/masx200/biliClient/BiliCall.kt
+++ src/main/kotlin/com/github/masx200/biliClient/BiliCall.kt
@@ -82,7 +82,7 @@ object BiliCall {
                         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
                     )
                     put("host", DomainExtractor.getDomainFromHttpRequest(httpRequest))
-                    put("Referer", httpRequest.uri.toString())
+//                    put("Referer", httpRequest.uri.toString())
                 }
             }
 
